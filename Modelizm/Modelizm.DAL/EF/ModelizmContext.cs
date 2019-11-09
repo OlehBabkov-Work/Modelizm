15a7fe8216144ccc22edd85520a0f98043a96598
OlehBabkov-Work <oleh.babkov@yahoo.com>
2019-11-09 23:37:52 +0200

    Context class is added

---
+++ Modelizm/Modelizm.DAL/EF/ModelizmContext.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Storage;
+using Modelizm.DAL.Entities;
+
+namespace Modelizm.DAL.EF
+{
+	// TODO: Don't forget to create initializer in presentation project
+	// TODO: Create DB Initializer class with mock entities
+	public class ModelizmContext : DbContext
+	{
+		public ModelizmContext(DbContextOptions<ModelizmContext> options) : base(options)
+		{ }
+
+		public DbSet<Model> Models { get; set; }
+		public DbSet<Company> Companies { get; set; }
+		public DbSet<Category> Categories { get; set; }
+		public DbSet<Order> Orders { get; set; }
+		public DbSet<Session> Sessions { get; set; }
+		public DbSet<User> Users { get; set; }
+	}
+}
